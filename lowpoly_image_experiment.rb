683041110c1293d1547829bc0332343490676bc2
Thomas Countz <thomascountz@gmail.com>
2023-07-18 17:32:44 +0200

    better random triangles

--- lowpoly_image_experiment.rb
+++ lowpoly_image_experiment.rb
@@ -24,24 +24,58 @@ Triangle = Data.define(
 end
 
 
-IMAGE_HEIGHT_PX = 100
-IMAGE_WIDTH_PX = 100
+IMAGE_HEIGHT_PX = 500
+IMAGE_WIDTH_PX = 500
 GREYSCALE_VALUES = (0..255).to_a
 POPULATION_SIZE = 5
-MIN_MEMBER_SIZE = 10
-MAX_MEMBER_SIZE = 75
+MIN_MEMBER_SIZE = 150
+MAX_MEMBER_SIZE = 300
+MIN_RADIUS = 20
+MAX_RADIUS = 80
 
 def random_triangle
+  # Choose a random point within the image
+  center_x, center_y = rand(IMAGE_WIDTH_PX), rand(IMAGE_HEIGHT_PX)
+  
+  # Create a circle around the point with a random radius
+  radius = rand(MIN_RADIUS..MAX_RADIUS)
+
+  # Choose three random points on the circle's circumference. `rand * 2 *
+  # Math::PI1 gives a random angle in the range `0` to `2 * Math::PI` radians,
+  # i.e., anywhere around the full circumference of the circle.  Then, given the
+  # angle and the radius, we can find the coordinates `(x, y)` on the circle by
+  # using the formulas `x = center_x + radius * Math.cos(angle)` and `y =
+  # center_y + radius * Math.sin(angle)`. These are basically the conversion
+  # formulas from polar to Cartesian coordinates.
+  angles = 3.times.map { rand * 2 * Math::PI }
+
+  points = angles.map do |angle|
+    x = center_x + radius * Math.cos(angle)
+    y = center_y + radius * Math.sin(angle)
+    [x, y]
+  end
+
   Triangle.new(
-    x1: rand(IMAGE_WIDTH_PX),
-    y1: rand(IMAGE_HEIGHT_PX),
-    x2: rand(IMAGE_WIDTH_PX),
-    y2: rand(IMAGE_HEIGHT_PX),
-    x3: rand(IMAGE_WIDTH_PX),
-    y3: rand(IMAGE_HEIGHT_PX),
+    x1: points[0][0], y1: points[0][1],
+    x2: points[1][0], y2: points[1][1],
+    x3: points[2][0], y3: points[2][1],
     grayscale: GREYSCALE_VALUES.sample
   )
 end
+
+# This implementation produced triangles that inherently avoided the border
+# and provided no controls of the size of the triangles
+# def random_triangle
+#   Triangle.new(
+#     x1: rand(IMAGE_WIDTH_PX),
+#     y1: rand(IMAGE_HEIGHT_PX),
+#     x2: rand(IMAGE_WIDTH_PX),
+#     y2: rand(IMAGE_HEIGHT_PX),
+#     x3: rand(IMAGE_WIDTH_PX),
+#     y3: rand(IMAGE_HEIGHT_PX),
+#     grayscale: GREYSCALE_VALUES.sample
+#   )
+# end
 
 def random_member
   Array.new(rand(MIN_MEMBER_SIZE..MAX_MEMBER_SIZE)) { random_triangle }
@@ -79,4 +113,5 @@ def import_image(path)
     .write("input_convert.png")
 end
 
+# import_image("astronaut.jpg")
 seed_population
